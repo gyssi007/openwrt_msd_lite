2655642895b51d9f36c672c87af86e8fdd04e992
william <gw826943555@qq.com>
2024-10-15 14:27:14 +0800

    luci: simpify status display

--- luci-app-msd_lite/htdocs/luci-static/resources/view/msd_lite/status.js
+++ luci-app-msd_lite/htdocs/luci-static/resources/view/msd_lite/status.js
@@ -8,6 +8,7 @@
 'require fs';
 'require poll';
 'require rpc'
+'require uci';
 'require ui';
 'require view';
 
@@ -18,57 +19,50 @@ var getStat = rpc.declare({
 });
 
 return view.extend({
-	load: function() {
-		return Promise.all([
-			getStat(),
-		]);
+	retrieveStatus: async function() {
+		return uci.load('msd_lite').then(function() {
+			var address = uci.get_first('msd_lite', 'instance', 'address');
+			var enabled = uci.get_first('msd_lite', 'instance', 'enabled');
+			var url = `http://${address[0]}/stat`
+
+			if(enabled == 0) {
+				return { value: 'msd lite is disabled.', rows:25 };
+			}
+
+			return fs.exec_direct('/usr/bin/wget', [ '-q', url, '-O', '-' ]).then(str => {
+				return { value: str, rows: str.split(/\r\n|\r|\n/).length};
+			});
+		});
 	},
 
-	pollData: function (container) {
-		poll.add(L.bind(function () {
-			return this.load().then(L.bind(function (data) {
-				dom.content(container, this.renderContent(data));
-			}, this));
-		}, this));
+	pollLog: async function() {
+		const element = document.getElementById('status');
+		if (element) {
+			const status = await this.retrieveStatus();
+			element.value = status.value;
+			element.rows = status.rows;
+		}
 	},
 
-	renderContent: function (results) {
-		var table = E('table', { 'class': 'table cbi-section-table', 'id': 'stat' }, [
-			E('tr', { 'class': 'tr table-titles' }, [
-				E('th', { 'class': 'th' }, _('Thread')),
-				E('th', { 'class': 'th' }, _('Clients Count')),
-				E('th', { 'class': 'th' }, _('Rate In(mbps)')),
-				E('th', { 'class': 'th' }, _('Rate Out(mbps)'))
+	load: async function() {
+		poll.add(this.pollLog.bind(this));
+		return await this.retrieveStatus();
+	},
+
+	render: function(status) {
+		return E([], [
+			E('div', { 'id': 'content_status' }, [
+				E('div', {'style': 'padding-bottom: 20px'}),
+				E('textarea', {
+					'id': 'status',
+					'style': 'width: 100% !important; padding: 5px; font-family: monospace',
+					'readonly': 'readonly',
+					'wrap': 'off',
+					'rows': status.rows,
+				}, [ status.value ]),
+				E('div', {'style': 'padding-bottom: 20px'})
 			])
 		]);
-
-		var clients = Array.isArray(results[0].clients) ? results[0].clients : [];
-		var ratein = Array.isArray(results[0].ratein) ? results[0].ratein : [];
-		var rateout = Array.isArray(results[0].rateout) ? results[0].rateout : [];
-		
-		let rows=new Array();
-		clients.forEach((c, i) => {
-			let row = [i, clients[i], ratein[i], rateout[i]];
-			rows.push(row.map(e => String(e)));
-		})
-
-		cbi_update_table(table, rows, E('em', _('MSD_Lite is not running.')));
-
-		return table;
-	},
-
-	render: function(results) {
-		var content = E([], [
-			E('h2', {class: 'content'}, _('Multi Stream daemon Lite')),
-			E('div', {class: 'cbi-map-descr'}, _('The lightweight version of Multi Stream daemon (msd) Program for organizing IP TV streaming on the network via HTTP.')),
-			E('div')
-		]);
-		var container = content.lastElementChild;
-
-		dom.content(container, this.renderContent(results));
-		this.pollData(container);
-
-		return content;
 	},
 
 	handleSaveApply: null,
